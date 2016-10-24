5cda8e6ecd0237883d55bc9bc67a2df9024279fc
Wolf <wolf.wikgren@gmail.com>
2016-10-24 23:12:33 +0300

    Initial

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var scraper = require('../scrapethestuff.js');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	res.render('index', { title: 'So you need a random dataset?' });
+});
+
+router.get('/wb', function(req, res, next) {
+	var wbLinks = scraper.scrapeIndicators();
+	wbLinks.then(function(result){
+		res.render('result', {title:'Result', link:result.link, url:result.url});
+	})
+});
+
+module.exports = router;
---
+++ scrapethestuff.js
@@ -0,0 +1,26 @@
+var Xray = require('x-ray');
+var x = Xray();
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+exports.scrapeIndicators = function(){
+	console.log("scrape thangs");
+	var foundLinks = [];
+	return new Promise(function(resolve, reject){
+		x('http://data.worldbank.org/indicator?tab=all', ['.overviewPage .wrapper .overviewArea .nav-item ul li'])(function(err, list){
+			x('http://data.worldbank.org/indicator?tab=all', ['.overviewPage .wrapper .overviewArea .nav-item ul li a@href'])(function(err, urls){
+				for(var i = 0; i < list.length; i++){
+					var obj = {'link': list[i], 'url': urls[i]};
+					foundLinks.push(obj);
+				}
+				var selector = getRandomInt(0, foundLinks.length);
+				console.log(selector);
+				resolve(foundLinks[selector]);
+			})
+		})
+	});
+}
